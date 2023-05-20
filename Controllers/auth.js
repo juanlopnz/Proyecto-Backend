2caf41507c8d03b4da8a7f5474fff1a83bd1609e
juanlopnz <jucalora17@gmail.com>
2023-05-20 01:55:12 -0500

    Pendiente models

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -1,14 +1,37 @@
 const express = require('express');
+const { validationResult } = require('express-validator');
 
 const crearUsuario = (req, res = express.response) => {
-  res.json({
+
+  const { username, email, password, confirmpassword  }  = req.body;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped()
+    });
+  }
+
+  if ( password !== confirmpassword ) {
+    return res.status(400).json({
+      ok: false,
+      msg: 'Las contraseñas no coinciden'
+    });
+  }
+
+  res.status(200).json({
     ok: true,
-    msg: 'Crear usuario /new'
+    msg: 'Crear usuario /new',
+    username, email, password 
   });
 }
 
 const loginUsuario = (req, res = express.response) => {
-  res.json({
+
+  const { username, password }  = req.body;
+
+  res.status(200).json({
     ok: true,
     msg: 'Login usuario /'
   });
--- Routes/auth.js
+++ Routes/auth.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const router = express.Router();
-
+const { check } = require('express-validator');
 const {crearUsuario, loginUsuario, revalidarToken} = require('../Controllers/auth');
 
+router.post(
+  '/new', 
+  [  
+    check('username', 'El nombre de usuario es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'La contraseña debe tener al menos 6 caracteres').isLength({ min: 6 }),
+    check('confirmpassword', ).isLength({ min: 6 }),
+  ],
+  crearUsuario );
+
 router.post('/', loginUsuario);
-router.post('/new', crearUsuario);
+
 router.get('/renew', revalidarToken);
 
 module.exports = router;
