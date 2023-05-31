c6973b4f7a56652fd59ebd49eb1e1bf69536adf0
juanlopnz <jucalora17@gmail.com>
2023-05-30 20:14:37 -0500

    Ultima actualización 5

--- Routes/auth.js
+++ Routes/auth.js
@@ -10,7 +10,7 @@ router.post(
     check('username', 'El nombre de usuario es obligatorio').not().isEmpty(),
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'La contraseña debe tener al menos 6 caracteres').isLength({ min: 6 }),
-    check('confirmpassword', ).isLength({ min: 6 }),
+    check('confirmpassword', ).isLength({ min: 6}),
     validarCampos
   ],
   crearUsuario );
@@ -18,7 +18,7 @@ router.post(
 router.post(
   '/', 
   [
-    check('username', 'El nombre de usuario es obligatorio').not().isEmpty(),
+    check('email', 'El correo electronico es obligatorio').not().isEmpty(),
     check('password', 'La contraseña es obligatoria').not().isEmpty(),
     validarCampos
   ],
